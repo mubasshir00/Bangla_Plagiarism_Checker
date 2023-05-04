c5637e880928cc1d57957faf46eb3410e708277b
mubasshir00 <marakib178@gmail.com>
2023-05-05 01:16:15 +0600

    model deployment and api calling from flask complete

--- article_categorization/app.py
+++ article_categorization/app.py
@@ -1,10 +1,12 @@
-import flask
-import pandas as pd
-import tensorflow as tf
+import flask 
+from flask import request, jsonify
+import pickle
 import keras
 from keras.models import load_model
+import json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-# instantiate flask 
+
 app = flask.Flask(__name__)
 
 # we need to redefine our metric function in order 
@@ -15,29 +17,37 @@ def auc(y_true, y_pred):
     return auc
 
 # load the model, and pass in the custom metric function
-global graph
-graph = tf.compat.v1.get_default_graph()
-model = load_model('./../data/Document_Test.h5', custom_objects={'auc': auc})
 
-# define a predict function as an endpoint 
-@app.route("/predict", methods=["GET","POST"])
+model = load_model('Document.h5', custom_objects={'auc': auc})
+
+
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    print(model)
-    data = {"success": False}
+    content_type = request.headers.get('Content-Type')
+    json =''
+    if (content_type == 'application/json'):
+        json = request.json
+    print(json['article'])    
+    sample_news = json['article']
+    class_names = ['Accident' ,'Amusement' ,'Art' ,'Business' ,'Chakri', 'Corporate', 'Employment','Health', 'Lifestyle', 'Opinion' ,'Politics' ,'Sports']
+    with open('tokenizer.pickle', 'rb') as handle:
+        loaded_tokenizer = pickle.load(handle)
+    seq= loaded_tokenizer.texts_to_sequences([sample_news])
+    padded = pad_sequences(seq, value=0.0,padding='post', maxlen= 300 )
 
-    params = flask.request.json
-    if (params == None):
-        params = flask.request.args
 
-    # if parameters are found, return a prediction
-    if (params != None):
-        x=pd.DataFrame.from_dict(params, orient='index').transpose()
-        with graph.as_default():
-            data["prediction"] = str(model.predict(x)[0][0])
-            data["success"] = True
+    pred = model.predict(padded)
+    y_pred = pred.reshape(-1)
+    class_labels=[(class_names[i],round(prob*100,2)) for i,prob in enumerate(y_pred) if prob > 0.1]
+    maxium = 0
+    category = "uncategorized"
+    for k, v in class_labels:
+        if maxium <= v :
+            maxium = v 
+            category = k
 
-    # return a response in json format 
-    return flask.jsonify(data)   
+    return flask.jsonify({'category': category});    
 
-if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+# start the flask app, allow remote connections 
+app.run(host='0.0.0.0',port=8080)
